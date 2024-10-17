234af24e447eb7d05dd76309b9581bf90de0ee64
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-10-17 21:03:52 +0200

    Trying to fix 404 issue on build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 import restart from "vite-plugin-restart";
 
-export default {
+export default defineConfig({
   root: "src/", // Sources files (typically where index.html is)
   publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
   server: {
@@ -12,9 +14,18 @@ export default {
     outDir: "../dist", // Output in the dist/ folder
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        artcube: resolve(__dirname, "src/art-cube.html"),
+        artspiral: resolve(__dirname, "src/art-spiral.html"),
+        arttiles: resolve(__dirname, "src/art-tiles.html"),
+        arttunnel: resolve(__dirname, "src/art-tunnel.html"),
+      },
+    },
   },
   plugins: [
     restart({ restart: ["../static/**"] }), // Restart server on static file change
     glsl(), // Handle GLSL files
   ],
-};
+});
